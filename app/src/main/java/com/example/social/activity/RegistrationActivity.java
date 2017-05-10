578e27416a83509da9b80c9427431202d4aefb16
Maxenss <kokayni4@gmail.com>
2017-05-10 15:55:32 +0300

    Допилена полностью регистрация

--- app/src/main/java/com/example/social/activity/RegistrationActivity.java
+++ app/src/main/java/com/example/social/activity/RegistrationActivity.java
@@ -1,5 +1,6 @@
 package com.example.social.activity;
 
+import android.os.AsyncTask;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
@@ -9,7 +10,16 @@ import android.widget.LinearLayout;
 import android.widget.Toast;
 
 import com.example.social.R;
-import com.example.social.classes.APIWorker;
+import com.example.social.classes.Data;
+import com.google.gson.GsonBuilder;
+
+import java.io.BufferedReader;
+import java.io.DataOutputStream;
+import java.io.InputStreamReader;
+import java.net.HttpURLConnection;
+import java.net.URL;
+import java.util.HashMap;
+import java.util.Map;
 
 public class RegistrationActivity extends AppCompatActivity {
 
@@ -29,6 +39,12 @@ public class RegistrationActivity extends AppCompatActivity {
     String firstName;
     String lastName;
     String middleName;
+
+    String requsetData;
+    String responseData;
+
+    int responseCode;
+    boolean isRegistered = false;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -98,25 +114,101 @@ public class RegistrationActivity extends AppCompatActivity {
             return;
         }
 
+        if (!userPassword.equals(passwordConfirm)){
+            Toast.makeText(this, "Пароли должны совпадать", Toast.LENGTH_SHORT).show();
+            return;
+        }
+
         // --------------------------------------------------------------//
         //                      Подключаемся к серверу
         // --------------------------------------------------------------//
 
-        llRegistration.setEnabled(false);
-
         try {
-            if (APIWorker.registationMethod(userLogin, userPassword, passwordConfirm,
-                    firstName, lastName, middleName))
-                Toast.makeText(this, "Регистрация прошла успешно", Toast.LENGTH_SHORT).show();
-            else {
-                Toast.makeText(this, "Что-то пошло не так", Toast.LENGTH_SHORT).show();
-            }
-        } catch (Exception e) {
+            registationMethod();
+        }
+        catch (Exception e){
             e.printStackTrace();
-            Toast.makeText(this, "Что-то пошло не так", Toast.LENGTH_SHORT).show();
+            Toast.makeText(RegistrationActivity.this, "Ошибка на этапе регистрации", Toast.LENGTH_SHORT).show();
+        }
+    }
+
+    private void registationMethod() throws Exception {
+        llRegistration.setEnabled(false);
+        Map<String, String> hashMapParams = new HashMap<>();
+
+        hashMapParams.put("login", userLogin);
+        hashMapParams.put("password", userPassword);
+        hashMapParams.put("passwordConfirm", passwordConfirm);
+        hashMapParams.put("firstName", firstName);
+        hashMapParams.put("lastName", lastName);
+        hashMapParams.put("middleName", middleName);
+
+        requsetData = new GsonBuilder().create().toJson(hashMapParams, Map.class);
+
+        RegistrationTask pt = new RegistrationTask();
+        pt.execute();
+    }
+
+    // HTTP Post request
+    private String makeRequest(String url, String postJsonData) throws Exception {
+        URL obj = new URL(url);
+        HttpURLConnection con = (HttpURLConnection) obj.openConnection();
+        responseData = "";
+
+        // Setting basic post request
+        con.setRequestMethod("POST");
+        con.setRequestProperty("Content-Type", "application/json");
+
+        // Send post request
+        con.setDoOutput(true);
+        DataOutputStream wr = new DataOutputStream(con.getOutputStream());
+        wr.writeBytes(postJsonData);
+        wr.flush();
+        wr.close();
+
+        int responceCode = con.getResponseCode();
+        System.out.println("nSending 'POST' request to URL : " + url);
+        System.out.println("Post Data : " + postJsonData);
+        System.out.println("Response Code : " + responceCode);
+
+        BufferedReader in = new BufferedReader(
+                new InputStreamReader(con.getInputStream()));
+        String output;
+        StringBuffer response = new StringBuffer();
+
+        while ((output = in.readLine()) != null) {
+            response.append(output);
+        }
+        in.close();
+
+        responseData = response.toString();
+        responseCode = responceCode;
+
+        System.out.println(responseData);
+        return responseData;
+    }
+
+    private class RegistrationTask extends AsyncTask<Void, Void, Void> {
+        @Override
+        protected Void doInBackground(Void... params) {
+            try {
+                makeRequest(Data.URL + "register", requsetData);
+                isRegistered = true;
+            } catch (Exception e) {
+                e.printStackTrace();
+                isRegistered = false;
+            }
+            return null;
         }
 
-        llRegistration.setEnabled(true);
-
+        @Override
+        protected void onPostExecute(Void res) {
+           // super.onPostExecute(res);
+            llRegistration.setEnabled(true);
+            if(isRegistered)
+                Toast.makeText(RegistrationActivity.this, "Регистрация прошла успешно", Toast.LENGTH_SHORT).show();
+            else
+                Toast.makeText(RegistrationActivity.this, "Ошибка на этапе регистрации", Toast.LENGTH_SHORT).show();
+        }
     }
 }
